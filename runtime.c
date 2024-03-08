66c55963fec1cdeac54a34560f394a34ecc6480a
Nils <datoxx8@gmail.com>
2024-03-08 22:20:38 +0100

    Improved runtime type and basic structure.

--- runtime.c
+++ runtime.c
@@ -3,6 +3,64 @@
 #include "linearize.h"
 #include "runtime.h"
 
-extern runtime_t runtime_allocate(enum runtime_e type, linearized_t *linearized);
-extern void runtime_execute(runtime_t *runtime);
-extern void runtime_free(runtime_t *runtime);
+/* TODO: How to get the layer starting points and sizes? */
+static void runtime_compile_linearized_(runtime_t *runtime, linearized_t *linearized, uint64_t section_start_i, uint64_t section_length) {
+}
+static void runtime_run_c_(runtime_t *runtime) {
+    for(uint64_t i = 0; i < runtime->linearized->op_count; i++) {
+        simple_op_realize(&runtime->linearized->simple[i]);
+    }
+}
+static void runtime_compile_layer_primitives_(runtime_t *runtime) {
+}
+runtime_t runtime_allocate(enum runtime_e type, linearized_t *linearized) {
+    runtime_t runtime = {0};
+    switch(type) {
+        case(runtime_c): {
+            runtime.type = runtime_c;
+            runtime.linearized = linearized;
+            break;
+        }
+        case(runtime_layer): {
+            runtime.type = runtime_layer;
+            break;
+        }
+        case(runtime_compile_layer): {
+            runtime.type = runtime_compile_layer;
+            break;
+        }
+        case(runtime_compile_sector): {
+            /* TODO: Hmmm... How do I do this splitting into subsections for this? Max layers? Max operations? */
+            runtime.type = runtime_compile_sector;
+            break;
+        }
+        case(runtime_compile_nn): {
+            runtime.type = runtime_compile_nn;
+            break;
+        }
+    }
+    return(runtime);
+}
+void runtime_free(runtime_t *runtime) {
+}
+void runtime_execute(runtime_t *runtime) {
+    switch(runtime->type) {
+        case(runtime_c): {
+            runtime_run_c_(runtime);
+            break;
+        }
+        case(runtime_layer): {
+            break;
+        }
+        case(runtime_compile_layer): {
+            break;
+        }
+        case(runtime_compile_sector): {
+            /* TODO: Hmmm... How do I do this splitting into subsections for this? Max layers? Max operations? */
+            break;
+        }
+        case(runtime_compile_nn): {
+            break;
+        }
+    }
+}
--- runtime.h
+++ runtime.h
@@ -1,12 +1,11 @@
 /*
-O fTIONS TO RUN LINEARIZED OPS:
-    1. C (ON THE CPU)
-    2. JIT
-    3. NAIVE BY LAYER
-    4. COMPILE
-        4.1 BY LAYER
-        4.2 BY SECTOR
-        4.3 BY NN
+OPTIONS TO RUN LINEARIZED OPS:
+    1. C ON THE CPU
+    2. NAIVE BY LAYER
+    3. COMPILE
+        3.1 BY LAYER
+        3.2 BY SECTOR
+        3.3 BY NN
  */
 #ifndef RUN_H_
 #define RUN_H_
@@ -19,17 +18,28 @@ O fTIONS TO RUN LINEARIZED OPS:
 
 /* NOTE: If run_compile_sector is chosen, then the program will automatically infer the size of the sectors. */
 enum runtime_e {
-    runtime_c, runtime_jit, runtime_layer, runtime_compile_layer, runtime_compile_sector, runtime_compile_nn
+    runtime_c, runtime_layer, runtime_compile_layer, runtime_compile_sector, runtime_compile_nn
 };
 
 typedef struct {
     enum runtime_e type;
+    /* C */
+    linearized_t *linearized;
+
+    /* COMPILED */
     uint64_t number_of_subprograms;
-    cl_program *compiled;
+    cl_kernel *kernel;
+    /* TODO: Check if these are backed into the cl_program. May have to store them anyway to free them when calling runtime_free. */
+    
+    cl_program *program;
+    cl_context *context;
+    cl_device_id *device;
+    cl_command_queue *queue;
 } runtime_t;
 
 extern runtime_t runtime_allocate(enum runtime_e type, linearized_t *linearized);
 extern void runtime_execute(runtime_t *runtime);
 extern void runtime_free(runtime_t *runtime);
+
 
 #endif
