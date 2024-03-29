c22dbae7542ac570f77ece3b19fb22c9f8329fca
Nils <datoxx8@gmail.com>
2024-03-29 13:34:59 +0100

    style: small change

--- utils.h
+++ utils.h
@@ -14,7 +14,7 @@ struct timespec stop = {0}; \
 #define STOP_TIME clock_gettime(CLOCK_REALTIME, &stop);
 
 #define PRINT_TIME \
-printf("TIME: %.9lfs\n", ((double)stop.tv_sec + 1.0e-9*stop.tv_nsec) - ((double)start.tv_sec + 1.0e-9*start.tv_nsec)); \
+printf("TIME: %.9lfs\n", ((double) stop.tv_sec + 1.0e-9 * stop.tv_nsec) - ((double) start.tv_sec + 1.0e-9 * start.tv_nsec)); \
 printf("TIME:   %*smmmµµµnnn\n", (int) (floor(log10(stop.tv_sec - start.tv_sec))), ""); \
 
 #endif
