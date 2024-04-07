d379e9e6908c86d0dd838acc616ce27bc4eca443
Nils <datoxx8@gmail.com>
2024-04-07 13:10:28 +0200

    feat: add TODO macro

--- utils.h
+++ utils.h
@@ -6,16 +6,16 @@
 #include <time.h>
 #define ALWAYS_INLINE inline __attribute__((always_inline))
 
-#define INIT_TIMER                                                                                                                                             \
+#define INIT_TIMER()                                                                                                                                           \
     struct timespec start = {0};                                                                                                                               \
     struct timespec stop = {0};
 
-#define START_TIME clock_gettime(CLOCK_REALTIME, &start);
-#define STOP_TIME clock_gettime(CLOCK_REALTIME, &stop);
+#define START_TIME() clock_gettime(CLOCK_REALTIME, &start);
+#define STOP_TIME() clock_gettime(CLOCK_REALTIME, &stop);
 
-#define PRINT_TIME                                                                                                                                             \
+#define PRINT_TIME()                                                                                                                                           \
     printf("TIME: %.9lfs\n", ((double)stop.tv_sec + 1.0e-9 * stop.tv_nsec) - ((double)start.tv_sec + 1.0e-9 * start.tv_nsec));                                 \
     printf("TIME:   %*smmmµµµnnn\n", (int)(floor(log10(stop.tv_sec - start.tv_sec))), "");
-#define TODO() fprintf(stderr, "ERROR: Tried to execute not implemented feature at line %d in %s\n", __LINE__, __FILE__);
+#define TODO() fprintf(stderr, "ERROR: Tried to execute not implemented feature at line %d in file %s\n", __LINE__, __FILE__);
 
 #endif
