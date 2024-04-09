52e3746af42b02b2e88bd29d08aed082e8b7752e
Nils <datoxx8@gmail.com>
2024-04-09 18:48:31 +0200

    chore: remove build files

--- debug.sh
+++
@@ -1,3 +0,0 @@
-set -xe
-clang main.c linearize.c tensor.c nn.c compile.c -o grad -ggdb -lm -lOpenCL -Wall -Wextra -pedantic -fsanitize=address
-./grad
--- fast.sh
+++
@@ -1,3 +0,0 @@
-set -xe
-clang main.c linearize.c tensor.c nn.c compile.c -o grad -Ofast -mavx2 -mbmi2 -lm -lOpenCL
-./grad
