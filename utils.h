79e851235a9dcb8fb65e10da7feb7fba41199d38
Nils <datoxx8@gmail.com>
2024-04-12 15:38:57 +0200

    fix: indenting of `TIME`
    Execution times close to 9 seconds would not indent correctly.

--- utils.h
+++ utils.h
@@ -13,14 +13,21 @@
 #define START_TIME() clock_gettime(CLOCK_REALTIME, &start);
 #define STOP_TIME() clock_gettime(CLOCK_REALTIME, &stop);
 
+/* TODO: Refactor this. This is straight up horrible code. */
 #define PRINT_TIME()                                                                                                                                           \
-    printf("TIME: %.9lfs\n", ((double)stop.tv_sec + 1.0e-9 * stop.tv_nsec) - ((double)start.tv_sec + 1.0e-9 * start.tv_nsec));                                 \
-    printf("TIME:   %*smmmµµµnnn\n", (int)(floor(log10(stop.tv_sec - start.tv_sec))), "");
+    printf("TIME: %.9lfs\n", ((double) stop.tv_sec + 1.0e-9 * stop.tv_nsec) - ((double) start.tv_sec + 1.0e-9 * start.tv_nsec));                               \
+    if((int) (((double) stop.tv_sec + 1.0e-9 * stop.tv_nsec) - ((double) start.tv_sec + 1.0e-9 * start.tv_nsec)) &&                                                  \
+       ((int) log10(((double) stop.tv_sec + 1.0e-9 * stop.tv_nsec) - ((double) start.tv_sec + 1.0e-9 * start.tv_nsec)))) {                                     \
+        printf("TIME:   %*smmmµµµnnn\n", (int) log10(((double) stop.tv_sec + 1.0e-9 * stop.tv_nsec) - ((double) start.tv_sec + 1.0e-9 * start.tv_nsec)), "");  \
+    } else {                                                                                                                                                   \
+        printf("TIME:   mmmµµµnnn\n");                                                                                                                         \
+    }
+
 #define TODO()                                                                                                                                                 \
     fprintf(stderr, "ERROR: Tried to execute not implemented feature at line %d in file %s\n", __LINE__, __FILE__);                                            \
     exit(1);
-#define ERROR(...)                                                                                                                                          \
-    fprintf(stderr, __VA_ARGS__);                                                                                                                                 \
+#define ERROR(...)                                                                                                                                             \
+    fprintf(stderr, __VA_ARGS__);                                                                                                                              \
     exit(1);
 
 #endif
