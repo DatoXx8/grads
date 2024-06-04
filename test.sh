fcec375019f7e46b51392fbcf0b5b593521c01ef
DatoXx8 <datoxx8@gmail.com>
2024-06-05 01:05:48 +0200

    test: ramp up the compiler testing

--- test.sh
+++ test.sh
@@ -1,9 +1,8 @@
-set -xe
 zig cc ./tensor.c ./tests/simulate-ops-cpu.c -o simulate-ops-cpu -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
 ./simulate-ops-cpu
 zig cc ./tensor.c ./tests/simulate-tree.c -o simulate-tree -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
 # ./simulate-tree 1000 10 1000
 ./simulate-tree 1000 10 2
 zig cc ./tensor.c ./compile.c ./runtimes/cl.c ./tests/simulate-compiler.c -o simulate-compiler -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
-# ./simulate-compiler 1000 10 2
-./simulate-compiler 1 10 1
+./simulate-compiler 1000 10 2
+# ./simulate-compiler 2 10 1
