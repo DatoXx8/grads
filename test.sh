65259a4ed6cb5968769d0c712e7e97966595c773
DatoXx8 <datoxx8@gmail.com>
2024-06-26 14:53:48 +0200

    chore: forgot to have the new name

--- test.sh
+++ test.sh
@@ -1,6 +1,6 @@
 zig cc ./tensor.c ./tests/simulate-ops-cpu.c -o simulate-ops-cpu -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
 ./simulate-ops-cpu
-zig cc ./tensor.c ./tests/simulate-linear.c -o simulate-linear -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
-./simulate-linear 1000 10
+zig cc ./tensor.c ./tests/simulate-linearized.c -o simulate-linearized -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
+./simulate-linearized 1000 10
 zig cc ./tensor.c ./compile.c ./runtimes/cl.c ./tests/simulate-compiler.c -o simulate-compiler -lm -lOpenCL -Wall -Wextra -pedantic -ggdb
 ./simulate-compiler 1000 10
