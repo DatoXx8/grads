5605b09dfe8c4a7f97374a64eeab857417eb2091
DatoXx8 <datoxx8@gmail.com>
2024-10-04 17:09:58 +0200

    Update TODOs

--- main.c
+++ main.c
@@ -12,13 +12,10 @@
 #include "utils.h"
 
 /*
- *  TODO: Make loops more abundant in the compiler simulator
- *  TODO: Support `global_size > 1`. surprisingly not trivial cuz of race conditions. I kinda think this has to be done
- * by splitting it up through multiple kernels such that I can control the snyc up. Auto detect when ops depend on each
- * other (Also make use of splitting ops up instead of doing stuff like `if(gid < x)` whenever possible)
- *  -> For now 1 kernel per loop. This could probably be optimised
- *  TODO: Make multiple cases for when `+=` can be used (if inline_num is == 1 I think it can be used otherwise I don't
- * think so)
+ *  TODO: Rewrite the compiler entirely and move that into a compiler/ directory instead of those two files
+ *  TODO: Rewrite test infrastructure to automatically minify the tests (fewest ops, cut from beginning if possible etc), produce logs
+ *  TODO: Make nicer logging with things like %04lu and things like that
+ *  TODO: Swarm testing
  *  TODO: Add multi-thread c runtime
  *  TODO: Make reduce backprop real and not fake.
  *  TODO: Maybe remove explicit backprop and make autograd things.
@@ -35,11 +32,6 @@
  *  TODO: Make a go engine.
  *  TODO: Make string helper functions in a string.c and string.h (Should probably do this first to avoid a bunch of
  * string weirdnes when I forget to remove a `*` somewhere and have another debugging nightmare)
- *
- * perf for 1e6 (cl, c unoptimized, c optimized):
- * (37s,128s,5s) baseline
- * (37s,128s,5s) use `+=` and stuff (I think this is optimized by the compiler anyway)
- * (48s,128s,5s) split up to multiple kernels (Slow at first but allows for more work-groups -> more work-items)
  */
 
 int main(int argc, const char **argv) {
@@ -78,7 +70,7 @@ int main(int argc, const char **argv) {
 
     const double LEARNING = 1e-2;
     const uint64_t SAMPLES = 1;
-    const uint64_t LAYERS = 4;
+    const uint64_t LAYERS = 5;
     const uint64_t INPUT_Z = 2;
     const uint64_t INPUT_Y = 4;
     const uint64_t INPUT_X = INPUT_Y;
@@ -114,14 +106,14 @@ int main(int argc, const char **argv) {
     layerconfig_t l4 = {
         .layer_type = layer_dense,
         .norm_type = norm_none,
-        .dense_output_size = 3,
+        .dense_output_size = 32,
         .activation_function = activation_none,
     };
     layerconfig[0] = l0;
     layerconfig[1] = l1;
     layerconfig[2] = l2;
     layerconfig[3] = l3;
-    // layerconfig[4] = l4;
+    layerconfig[4] = l4;
 
     neuralnet_t neuralnet = neuralnet_alloc(LAYERS, layerconfig, LEARNING, compile_type);
     tensor_t input = tensor_alloc(SAMPLES, NEURALNET_INPUT(neuralnet).activation->buffer->sze_z,
@@ -136,10 +128,14 @@ int main(int argc, const char **argv) {
     tensor_realize(&input);
     neuralnet_random(&neuralnet);
 
-    // for(uint64_t i = 0; i < 1e6; i++) {
     neuralnet_forward(&neuralnet, &input);
-    // }
     TENSOR_PRINT_(NEURALNET_OUTPUT(neuralnet).activation);
+    for(uint64_t i = 0; i < 1e3; i++) {
+        neuralnet_backward(&neuralnet, &input, &output);
+        neuralnet_learn(&neuralnet);
+    }
+    TENSOR_PRINT_(NEURALNET_OUTPUT(neuralnet).activation);
+    TENSOR_PRINT(output);
     for(uint64_t kernel_idx = 0; kernel_idx < neuralnet.forward_cl.kernel_num; kernel_idx++) {
         printf("%s\n", neuralnet.forward_cl.kernel[kernel_idx].source);
     }
