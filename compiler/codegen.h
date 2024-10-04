85393cc963a21d7a93d0055289671cf001be46db
DatoXx8 <datoxx8@gmail.com>
2024-10-04 19:43:59 +0200

    Add option primitive for all optimizations

--- compiler/codegen.h
+++ compiler/codegen.h
@@ -4,8 +4,9 @@
 #include "compile.h"
 #include <stdint.h>
 
+/* Bitwise or these together to decide the optimization options. Optimizations may be ignored if they are likely to
+ * introduce bugs into the kernel */
 const uint64_t optimization_none = 0;
-/* Bitwise or these together to decide the optimization options */
 const uint64_t optimization_unroll = (1 << 0); /* Unroll loops */
 const uint64_t optimization_inline = (1 << 1); /* Inline ops i.e. `a += b; a *= c` -> `a = (a + b) * c` */
 const uint64_t optimization_split = (1 << 2);  /* Split singular ops between kernels */
@@ -15,6 +16,8 @@ const uint64_t optimization_fuse_y = (1 << 5); /* Aggregate along axis y using s
 const uint64_t optimization_fuse_x = (1 << 6); /* Aggregate along axis x using simd types like float4 */
 const uint64_t optimization_memory = (1 << 7); /* Try to optimize memory accesses and cache. May be slow initially */
 const uint64_t optimization_kernel = (1 << 8); /* Reduce number of kernels being sent to the GPU */
+const uint64_t optimization_all =
+    UINT64_MAX; /* All bits set to 1 -> All optimizations on. Significantly icreases compile times */
 extern char *compile_op_group(op_group_t *group, uint64_t optimization);
 
 #endif
