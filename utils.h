b53c4400541d8c9c0e96bb93ed11472ce68a50ab
DatoXx8 <datoxx8@gmail.com>
2024-10-07 09:17:10 +0200

    Add output parameter macro and timers

--- utils.h
+++ utils.h
@@ -3,16 +3,14 @@
 #define CGRAD_UTILS_H_
 
 #include "math.h"
+#include <stdint.h>
+
+#define output /* Macro to mark output parameters */
 
 #define ALWAYS_INLINE inline __attribute__((always_inline))
 
-/* TODO: Have different timers with `INIT_TIMER(name)` */
-#define INIT_TIMER()                                                                                                   \
-    struct timespec start = {0};                                                                                       \
-    struct timespec stop = {0};
-
-#define START_TIME() clock_gettime(CLOCK_REALTIME, &start);
-#define STOP_TIME() clock_gettime(CLOCK_REALTIME, &stop);
+extern void time_ns_store(const uint64_t id);
+extern uint64_t time_ns_load(const uint64_t id);
 
 #define PRINT_TIME(name)                                                                                               \
     printf("TIME: %.9lfs for " name "\n",                                                                              \
