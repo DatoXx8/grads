3d6d6fa459f69da81bc2204919d8cf245f72ed04
DatoXx8 <datoxx8@gmail.com>
2024-10-07 11:40:36 +0200

    More accurate comments

---
+++ compiler/codegen.c
@@ -0,0 +1,7 @@
+#include "compile.h"
+
+void compile_op_group(kernel_t *kernel, const op_group_t *group, const uint64_t optimization) {
+    (void) kernel;
+    (void) group;
+    (void) optimization;
+}
--- compiler/codegen.h
+++ compiler/codegen.h
@@ -8,16 +8,16 @@
  * introduce bugs into the kernel */
 const uint64_t optimization_none = 0;
 const uint64_t optimization_unroll = (1 << 0); /* Unroll loops */
-const uint64_t optimization_inline = (1 << 1); /* Inline ops i.e. `a += b; a *= c` -> `a = (a + b) * c` */
+const uint64_t optimization_inline = (1 << 1); /* Inline ops i.e. `a += b; a *= c;` -> `a = (a + b) * c;` */
 const uint64_t optimization_split = (1 << 2);  /* Split singular ops between kernels */
-const uint64_t optimization_fuse_a = (1 << 3); /* Aggregate along axis a using simd types like float4 */
-const uint64_t optimization_fuse_z = (1 << 4); /* Aggregate along axis z using simd types like float4 */
-const uint64_t optimization_fuse_y = (1 << 5); /* Aggregate along axis y using simd types like float4 */
-const uint64_t optimization_fuse_x = (1 << 6); /* Aggregate along axis x using simd types like float4 */
-const uint64_t optimization_memory = (1 << 7); /* Try to optimize memory accesses and cache. May be slow initially */
+const uint64_t optimization_fuse_a = (1 << 3); /* Aggregate along a-axis using simd types like float4 */
+const uint64_t optimization_fuse_z = (1 << 4); /* Aggregate along z-axis using simd types like float4 */
+const uint64_t optimization_fuse_y = (1 << 5); /* Aggregate along y-axis using simd types like float4 */
+const uint64_t optimization_fuse_x = (1 << 6); /* Aggregate along x-axis using simd types like float4 */
+const uint64_t optimization_memory = (1 << 7); /* Try to optimize memory accesses and cache. Expensive to compile */
 const uint64_t optimization_kernel = (1 << 8); /* Reduce number of kernels being sent to the GPU */
-const uint64_t optimization_all =
-    UINT64_MAX; /* All bits set to 1 -> All optimizations on. May significantly icreases compile times */
-extern char *compile_op_group(op_group_t *group, uint64_t optimization);
+/* All bits set to 1 -> All optimizations on. May significantly increase compile times */
+const uint64_t optimization_all = UINT64_MAX;
+extern void compile_op_group(kernel_t *kernel, const op_group_t *group, const uint64_t optimization);
 
 #endif
