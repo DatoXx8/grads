845a84d57ff2a221ce3a735c36476fa83e2d8c50
DatoXx8 <datoxx8@gmail.com>
2024-10-14 09:40:39 +0200

    Compile kernel start and end + memory bugfix

--- compiler/codegen.c
+++ compiler/codegen.c
@@ -1,6 +1,7 @@
 #include "codegen.h"
 #include <assert.h>
 #include <stdint.h>
+#include <stdio.h>
 #include <stdlib.h>
 
 #include "compile.h"
@@ -19,6 +20,7 @@ const uint64_t optimization_all = UINT64_MAX;  /* All optimizations */
 
 /* TODO: Choose reasonable limit based on some data and not just a gut feeling */
 const uint64_t padding = 1024;
+const uint64_t write_len_max = padding;
 /* The length of the source can just be calculated so there is no need to store that */
 static inline void source_expand(char **source, char **source_curr, uint64_t *source_cap) {
     /* MAYBE: Not sure if I should allow `*source` and things like that to be NULL so that this could be used to
@@ -41,7 +43,8 @@ static inline void source_expand(char **source, char **source_curr, uint64_t *so
     }
 }
 
-static void source_index_scheme(char **source, char **source_curr, uint64_t *source_cap) {
+static void source_append_kernel_start(char **source, char **source_curr, uint64_t *source_cap, const char **arg,
+                                       const uint64_t arg_num) {
     assert(source);
     assert(*source);
     assert(source_curr);
@@ -49,12 +52,118 @@ static void source_index_scheme(char **source, char **source_curr, uint64_t *sou
     assert(*source <= *source_curr);
     assert(source_cap);
     assert(*source_cap);
+    assert(arg);
+    /* MAYBE: Assert all the strings in `arg` */
+    assert(arg_num);
+
+    *source_curr += snprintf(*source_curr, write_len_max, "__kernel void " KERNEL_NAME "(");
+    source_expand(source, source_curr, source_cap);
+    for(uint64_t arg_idx = 0; arg_idx < arg_num; arg_idx++) {
+        /* There might be some optimization cases where it's not a `__global` or `double` */
+        if(arg_idx) {
+            *source_curr += snprintf(*source_curr, write_len_max, ", __global double *%s", arg[arg_idx]);
+        } else {
+            *source_curr += snprintf(*source_curr, write_len_max, "__global double *%s", arg[arg_idx]);
+        }
+        source_expand(source, source_curr, source_cap);
+    }
+    *source_curr += snprintf(*source_curr, write_len_max, ") {\n");
+    source_expand(source, source_curr, source_cap);
 }
 
-void compile_op_group(kernel_t *kernel, const op_group_t *group, const uint64_t optimization) {
+static void source_append_kernel_end(char **source, char **source_curr, uint64_t *source_cap) {
+    assert(source);
+    assert(*source);
+    assert(source_curr);
+    assert(*source_curr);
+    assert(*source <= *source_curr);
+    assert(source_cap);
+    assert(*source_cap);
+
+    *source_curr += snprintf(*source_curr, write_len_max, "}\n");
+    source_expand(source, source_curr, source_cap);
+}
+
+static void source_append_head(char **source, char **source_curr, uint64_t *source_cap) {
+    assert(source);
+    assert(*source);
+    assert(source_curr);
+    assert(*source_curr);
+    assert(*source <= *source_curr);
+    assert(source_cap);
+    assert(*source_cap);
+
+    *source_curr += snprintf(*source_curr, write_len_max, "__local const int gid = get_global_id(0);\n");
+    source_expand(source, source_curr, source_cap);
+    *source_curr += snprintf(*source_curr, write_len_max, "__local int id;\n");
+    source_expand(source, source_curr, source_cap);
+}
+
+static void source_append_index(char **source, char **source_curr, uint64_t *source_cap, const op_t *op,
+                                const uint64_t op_idx, const uint64_t loop_idx) {
+    assert(source);
+    assert(*source);
+    assert(source_curr);
+    assert(*source_curr);
+    assert(*source <= *source_curr);
+    assert(source_cap);
+    assert(*source_cap);
+    assert(op);
+
+    /* TODO: Choose a more accurate name for what this is */
+    const uint64_t x_wait_out = 1;
+    const uint64_t y_wait_out = x_wait_out * op->buffer_out.x_sze;
+    const uint64_t z_wait_out = y_wait_out * op->buffer_out.y_sze;
+    const uint64_t a_wait_out = z_wait_out * op->buffer_out.z_sze;
+    *source_curr +=
+        snprintf(*source_curr, write_len_max,
+                 "__local int %s_%lu_%lu = %lu+id%%%lu/%lu*%lu+id%%%lu/%lu*%lu+id%%%lu/%lu*%lu+id%%%lu/%lu*%lu;\n",
+                 op->buffer_out.name, loop_idx, op_idx, op->buffer_out.off, op->buffer_out.a_sze * a_wait_out,
+                 a_wait_out, op->buffer_out.a_str, a_wait_out, z_wait_out, op->buffer_out.z_str, z_wait_out, y_wait_out,
+                 op->buffer_out.y_str, y_wait_out, x_wait_out, op->buffer_out.x_str);
+    source_expand(source, source_curr, source_cap);
+    if(op->type_op != op_unary) {
+        const uint64_t x_wait_in = 1;
+        const uint64_t y_wait_in = x_wait_in * op->buffer_in.x_sze;
+        const uint64_t z_wait_in = y_wait_in * op->buffer_in.y_sze;
+        const uint64_t a_wait_in = z_wait_in * op->buffer_in.z_sze;
+        *source_curr +=
+            snprintf(*source_curr, write_len_max,
+                     "__local int %s_%lu_%lu = %lu+id%%%lu/%lu*%lu+id%%%lu/%lu*%lu+id%%%lu/%lu*%lu+id%%%lu/%lu*%lu;\n",
+                     op->buffer_in.name, loop_idx, op_idx, op->buffer_in.off, op->buffer_in.a_sze * a_wait_in,
+                     a_wait_in, op->buffer_in.a_str, a_wait_in, z_wait_in, op->buffer_in.z_str, z_wait_in, y_wait_in,
+                     op->buffer_in.y_str, y_wait_in, x_wait_in, op->buffer_in.x_str);
+        source_expand(source, source_curr, source_cap);
+    }
+}
+void compile_op_group(kernel_t *kernel, const op_group_t *group, const uint64_t size_global, const uint64_t size_local,
+                      const uint64_t optimization) {
     assert(kernel);
     assert(group);
+    assert(size_global);
+    assert(size_local);
+    assert(size_local <= size_global);
+
     if(optimization != optimization_none) {
         TODO();
     }
+    uint64_t source_cap = padding;
+    char *source = calloc(source_cap, sizeof(char));
+    char *source_curr = source;
+
+    source_append_kernel_start(&source, &source_curr, &source_cap, (const char **) kernel->arg_name, kernel->arg_num);
+    source_append_head(&source, &source_curr, &source_cap);
+
+    const uint64_t loop_leftover = group->repeat_num % size_global;
+    const uint64_t loop_num = group->repeat_num / size_global + loop_leftover ? 1 : 0;
+    for(uint64_t loop_idx = 0; loop_idx < loop_num; loop_idx++) {
+        for(uint64_t op_idx = 0; op_idx < group->op_num; op_idx++) {
+            source_append_index(&source, &source_curr, &source_cap, &group->op[op_idx], op_idx, loop_idx);
+        }
+    }
+
+    source_append_kernel_end(&source, &source_curr, &source_cap);
+
+    kernel->source = source;
+    kernel->source_cap = source_cap;
 }
--- compiler/codegen.h
+++ compiler/codegen.h
@@ -6,6 +6,7 @@
 
 /* TODO: Option to choose smaller local and global size as an optimization or default? Does that even enable any
  * optimizations? */
+/* I think optimization_memory may be the only place where local_size matters */
 
 /* Bitwise or these together to decide the optimization options. */
 /* Values located in `codegen.c` */
@@ -20,6 +21,7 @@ extern const uint64_t optimization_fuse_x; /* Aggregate along x-axis using simd 
 extern const uint64_t optimization_memory; /* Try to optimize memory accesses and cache. Expensive to compile */
 extern const uint64_t optimization_kernel; /* Reduce number of kernels being sent to the GPU */
 extern const uint64_t optimization_all;    /* All optimizations */
-extern void compile_op_group(kernel_t *kernel, const op_group_t *group, const uint64_t optimization);
+extern void compile_op_group(kernel_t *kernel, const op_group_t *group, const uint64_t size_global,
+                             const uint64_t size_local, const uint64_t optimization);
 
 #endif
