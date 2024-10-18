b31b5d745b9eeb7dcec6d13f3db82115e3be40ce
DatoXx8 <datoxx8@gmail.com>
2024-10-18 09:39:21 +0200

    Use ERROR macro for UNREACHABLE and TODO

--- utils.h
+++ utils.h
@@ -19,15 +19,15 @@ extern uint64_t time_ns_load(const uint64_t id);
         printf("TIME:   mmmµµµnnn\n");                                                                                 \
     }
 
-#define UNREACHABLE()                                                                                                  \
-    fprintf(stderr, "ERROR: Reached `UNREACHABLE()` in %s at line %d at %s %s\n", __FILE__, __LINE__, __DATE__,        \
-            __TIME__);                                                                                                 \
-    exit(1);
-#define TODO()                                                                                                         \
-    fprintf(stderr, "ERROR: Tried to execute not implemented feature at line %d in file %s\n", __LINE__, __FILE__);    \
-    exit(1);
 #define ERROR(...)                                                                                                     \
     fprintf(stderr, "ERROR: " __VA_ARGS__);                                                                            \
+    exit(1);
+
+#define UNREACHABLE()                                                                                                  \
+    ERROR("Reached `UNREACHABLE()` in %s at line %d at %s %s\n", __FILE__, __LINE__, __DATE__, __TIME__);
+
+#define TODO()                                                                                                         \
+    ERROR("Tried to execute not implemented feature at line %d in file %s\n", __LINE__, __FILE__);             \
     exit(1);
 #define WARN(...)                                                                                                      \
     if(getenv("LOGGER") && getenv("LOGGER")[0] >= '1') {                                                               \
