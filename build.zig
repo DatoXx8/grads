624e36d3b2abbfccb134875a430ffc6075a616f1
DatoXx8 <datoxx8@gmail.com>
2024-11-05 08:41:29 +0100

    CHORE: Update name

--- build.zig
+++ build.zig
@@ -18,7 +18,7 @@ pub fn build(b: *std.Build) void {
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
-        .name = "cgrad",
+        .name = "grads",
         .root_source_file = b.path("src/main.zig"),
         .target = target,
         .optimize = optimize,
