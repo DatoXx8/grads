53d43b60ff657da4771f6a5e9c19e496dec69714
DatoXx8 <datoxx8@gmail.com>
2024-11-28 00:47:22 +0100

    Add unit tests for single ops to build

--- build.zig
+++ build.zig
@@ -57,17 +57,25 @@ pub fn build(b: *std.Build) void {
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
-    const exe_unit_tests = b.addTest(.{
-        .root_source_file = b.path("src/main.zig"),
+    const unit_test_op = b.addTest(.{
+        .name = "unit_test_op",
+        .root_source_file = b.path("src/unit-ops.zig"),
         .target = target,
         .optimize = optimize,
     });
+    unit_test_op.addIncludePath(.{
+        .cwd_relative = "/usr/include/",
+    });
+    unit_test_op.linkSystemLibrary("c");
+    unit_test_op.linkSystemLibrary("OpenCL");
 
-    const run_exe_unit_tests = b.addRunArtifact(exe_unit_tests);
+    const run_unit_tests = b.addRunArtifact(unit_test_op);
+    // I don't entirely undestand this but it makes the tests rerun if you run `zig build test` more than once
+    run_unit_tests.has_side_effects = true;
 
     // Similar to creating the run step earlier, this exposes a `test` step to
     // the `zig build --help` menu, providing a way for the user to request
     // running the unit tests.
     const test_step = b.step("test", "Run unit tests");
-    test_step.dependOn(&run_exe_unit_tests.step);
+    test_step.dependOn(&run_unit_tests.step);
 }
