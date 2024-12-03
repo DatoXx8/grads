a55badffc482292093b5ed7c517d326ffaa64e02
DatoXx8 <datoxx8@gmail.com>
2024-12-03 10:50:02 +0100

    Add build step for linearized tests

--- build.zig
+++ build.zig
@@ -1,20 +1,7 @@
 const std = @import("std");
 
-// Although this function looks imperative, note that its job is to
-// declaratively construct a build graph that will be executed by an external
-// runner.
 pub fn build(b: *std.Build) void {
-    // Standard target options allows the person running `zig build` to choose
-    // what target to build for. Here we do not override the defaults, which
-    // means any target is allowed, and the default is native. Other options
-    // for restricting supported target set are available.
     const target = b.standardTargetOptions(.{});
-
-    // Standard optimization options allow the person running `zig build` to select
-    // between Debug, ReleaseSafe, ReleaseFast, and ReleaseSmall. Here we do not
-    // set a preferred release mode, allowing the user to decide how to optimize.
-    //
-    // TODO: Disallow ReleaseFast because it removes assertions. Alternatively do an assert that still works
     const optimize = b.standardOptimizeOption(.{});
 
     const exe = b.addExecutable(.{
@@ -29,32 +16,12 @@ pub fn build(b: *std.Build) void {
     // TODO: Figure out how to get rid of libc
     exe.linkSystemLibrary("c");
     exe.linkSystemLibrary("OpenCL");
-
-    // This declares intent for the executable to be installed into the
-    // standard location when the user invokes the "install" step (the default
-    // step when running `zig build`).
     b.installArtifact(exe);
-
-    // This *creates* a Run step in the build graph, to be executed when another
-    // step is evaluated that depends on it. The next line below will establish
-    // such a dependency.
     const run_cmd = b.addRunArtifact(exe);
-
-    // By making the run step depend on the install step, it will be run from the
-    // installation directory rather than directly from within the cache directory.
-    // This is not necessary, however, if the application depends on other installed
-    // files, this ensures they will be present and in the expected location.
     run_cmd.step.dependOn(b.getInstallStep());
-
-    // This allows the user to pass arguments to the application in the build
-    // command itself, like this: `zig build run -- arg1 arg2 etc`
     if (b.args) |args| {
         run_cmd.addArgs(args);
     }
-
-    // This creates a build step. It will be visible in the `zig build --help` menu,
-    // and can be selected like this: `zig build run`
-    // This will evaluate the `run` step rather than the default, which is "install".
     const run_step = b.step("run", "Run the app");
     run_step.dependOn(&run_cmd.step);
 
@@ -70,19 +37,33 @@ pub fn build(b: *std.Build) void {
     // TODO: Figure out how to get rid of libc
     unit_test_op.linkSystemLibrary("c");
     unit_test_op.linkSystemLibrary("OpenCL");
-
     b.installArtifact(unit_test_op);
-
-    const run_unit_tests = b.addRunArtifact(unit_test_op);
-    run_unit_tests.step.dependOn(b.getInstallStep());
-
-    // Similar to creating the run step earlier, this exposes a `test` step to
-    // the `zig build --help` menu, providing a way for the user to request
-    // running the unit tests.
-    const test_step = b.step("test-op", "Run unit tests for singular ops");
-    test_step.dependOn(&run_unit_tests.step);
-
+    const test_op = b.addRunArtifact(unit_test_op);
+    test_op.step.dependOn(b.getInstallStep());
     if (b.args) |args| {
-        run_unit_tests.addArgs(args);
+        test_op.addArgs(args);
     }
+    const test_op_step = b.step("test-op", "Run unit tests for singular ops");
+    test_op_step.dependOn(&test_op.step);
+
+    const simulation_test_linearized = b.addExecutable(.{
+        .name = "simulate-linearized",
+        .root_source_file = b.path("src/simulate-linearized.zig"),
+        .target = target,
+        .optimize = optimize,
+    });
+    simulation_test_linearized.addIncludePath(.{
+        .cwd_relative = "/usr/include/",
+    });
+    // TODO: Figure out how to get rid of libc
+    simulation_test_linearized.linkSystemLibrary("c");
+    simulation_test_linearized.linkSystemLibrary("OpenCL");
+    b.installArtifact(simulation_test_linearized);
+    const test_linearized = b.addRunArtifact(simulation_test_linearized);
+    test_linearized.step.dependOn(b.getInstallStep());
+    if (b.args) |args| {
+        test_linearized.addArgs(args);
+    }
+    const test_linearized_step = b.step("test-linearized", "Run the simulator for linearized ops");
+    test_linearized_step.dependOn(&test_linearized.step);
 }
