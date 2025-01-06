6639959e3b77fb1657540d2383c494fb1d189132
DatoXx8 <datoxx8@gmail.com>
2025-01-06 20:01:31 +0100

    Switch to struct names

--- src/compiler/pir.zig
+++ src/compiler/pir.zig
@@ -45,7 +45,7 @@ pub const Pir = struct {
     pub fn alloc(allocator: anytype, linearized: Linearized, op_used: *u32) !Pir {
         assert(op_used.* < linearized.op_num);
         var op_num: u32 = 0;
-        var group_num: u32 = 1;
+        var repeat_num: u32 = 1;
         const op_start: u32 = op_used.*;
 
         for (1..linearized.op_num - op_start) |op_off| {
@@ -72,7 +72,7 @@ pub const Pir = struct {
         if (op_num == 0) {
             op_num = 1;
         } else {
-            const group_num_max: u32 = @truncate(@divFloor(linearized.op_num - op_num - op_start + 1, op_num));
+            const group_num_max: u32 = @truncate(@divFloor(linearized.op_num - (op_num + op_start), op_num) + 1);
             for (1..group_num_max) |group_idx| {
                 var all_equal: bool = true;
                 for (0..op_num) |inner_off| {
@@ -85,14 +85,14 @@ pub const Pir = struct {
                     }
                 }
                 if (all_equal) {
-                    group_num += 1;
+                    repeat_num += 1;
                 } else {
                     break;
                 }
             }
         }
-        const op: []Op = try allocator.alloc(Op, op_num * group_num);
-        const dim_info: []DimInfo = try allocator.alloc(DimInfo, op_num * group_num);
+        const op: []Op = try allocator.alloc(Op, op_num * repeat_num);
+        const dim_info: []DimInfo = try allocator.alloc(DimInfo, op_num * repeat_num);
         for (0..op_num) |op_idx| {
             op[op_idx] = linearized.op[op_start + op_idx];
         }
@@ -127,17 +127,18 @@ pub const Pir = struct {
             var y_in_reenter: bool = false;
             var x_in_reenter: bool = false;
 
-            for (0..group_num) |group_idx| {
+            for (0..repeat_num) |group_idx_usize| {
+                const group_idx: u32 = @truncate(group_idx_usize);
                 if (a_out_left) {
                     if (!a_out_reenter) {
                         if (linearized.op[op_start + op_idx + group_idx * op_num].out.a_offset == a_out_initial) {
-                            dim_info[op_idx].res_a_out = @truncate(group_idx);
+                            dim_info[op_idx].res_a_out = group_idx;
                             a_out_reenter = true;
                         }
                     }
                 } else {
                     if (linearized.op[op_start + op_idx + group_idx * op_num].out.a_offset != a_out_initial) {
-                        dim_info[op_idx].wai_a_out = @truncate(group_idx);
+                        dim_info[op_idx].wai_a_out = group_idx;
                         dim_info[op_idx].str_a_out = linearized.op[op_start + op_idx + group_idx * op_num].out.a_offset - a_out_initial;
                         a_out_left = true;
                     }
@@ -145,13 +146,13 @@ pub const Pir = struct {
                 if (z_out_left) {
                     if (!z_out_reenter) {
                         if (linearized.op[op_start + op_idx + group_idx * op_num].out.z_offset == z_out_initial) {
-                            dim_info[op_idx].res_z_out = @truncate(group_idx);
+                            dim_info[op_idx].res_z_out = group_idx;
                             z_out_reenter = true;
                         }
                     }
                 } else {
                     if (linearized.op[op_start + op_idx + group_idx * op_num].out.z_offset != z_out_initial) {
-                        dim_info[op_idx].wai_z_out = @truncate(group_idx);
+                        dim_info[op_idx].wai_z_out = group_idx;
                         dim_info[op_idx].str_z_out = linearized.op[op_start + op_idx + group_idx * op_num].out.z_offset - z_out_initial;
                         z_out_left = true;
                     }
@@ -159,13 +160,13 @@ pub const Pir = struct {
                 if (y_out_left) {
                     if (!y_out_reenter) {
                         if (linearized.op[op_start + op_idx + group_idx * op_num].out.y_offset == y_out_initial) {
-                            dim_info[op_idx].res_y_out = @truncate(group_idx);
+                            dim_info[op_idx].res_y_out = group_idx;
                             y_out_reenter = true;
                         }
                     }
                 } else {
                     if (linearized.op[op_start + op_idx + group_idx * op_num].out.y_offset != y_out_initial) {
-                        dim_info[op_idx].wai_y_out = @truncate(group_idx);
+                        dim_info[op_idx].wai_y_out = group_idx;
                         dim_info[op_idx].str_y_out = linearized.op[op_start + op_idx + group_idx * op_num].out.y_offset - y_out_initial;
                         y_out_left = true;
                     }
@@ -173,13 +174,13 @@ pub const Pir = struct {
                 if (x_out_left) {
                     if (!x_out_reenter) {
                         if (linearized.op[op_start + op_idx + group_idx * op_num].out.x_offset == x_out_initial) {
-                            dim_info[op_idx].res_x_out = @truncate(group_idx);
+                            dim_info[op_idx].res_x_out = group_idx;
                             x_out_reenter = true;
                         }
                     }
                 } else {
                     if (linearized.op[op_start + op_idx + group_idx * op_num].out.x_offset != x_out_initial) {
-                        dim_info[op_idx].wai_x_out = @truncate(group_idx);
+                        dim_info[op_idx].wai_x_out = group_idx;
                         dim_info[op_idx].str_x_out = linearized.op[op_start + op_idx + group_idx * op_num].out.x_offset - x_out_initial;
                         x_out_left = true;
                     }
@@ -187,13 +188,13 @@ pub const Pir = struct {
                 if (a_in_left) {
                     if (!a_in_reenter) {
                         if (linearized.op[op_start + op_idx + group_idx * op_num].in.a_offset == a_in_initial) {
-                            dim_info[op_idx].res_a_in = @truncate(group_idx);
+                            dim_info[op_idx].res_a_in = group_idx;
                             a_in_reenter = true;
                         }
                     }
                 } else {
                     if (linearized.op[op_start + op_idx + group_idx * op_num].in.a_offset != a_in_initial) {
-                        dim_info[op_idx].wai_a_in = @truncate(group_idx);
+                        dim_info[op_idx].wai_a_in = group_idx;
                         dim_info[op_idx].str_a_in = linearized.op[op_start + op_idx + group_idx * op_num].in.a_offset - a_in_initial;
                         a_in_left = true;
                     }
@@ -201,13 +202,13 @@ pub const Pir = struct {
                 if (z_in_left) {
                     if (!z_in_reenter) {
                         if (linearized.op[op_start + op_idx + group_idx * op_num].in.z_offset == z_in_initial) {
-                            dim_info[op_idx].res_z_in = @truncate(group_idx);
+                            dim_info[op_idx].res_z_in = group_idx;
                             z_in_reenter = true;
                         }
                     }
                 } else {
                     if (linearized.op[op_start + op_idx + group_idx * op_num].in.z_offset != z_in_initial) {
-                        dim_info[op_idx].wai_z_in = @truncate(group_idx);
+                        dim_info[op_idx].wai_z_in = group_idx;
                         dim_info[op_idx].str_z_in = linearized.op[op_start + op_idx + group_idx * op_num].in.z_offset - z_in_initial;
                         z_in_left = true;
                     }
@@ -215,13 +216,13 @@ pub const Pir = struct {
                 if (y_in_left) {
                     if (!y_in_reenter) {
                         if (linearized.op[op_start + op_idx + group_idx * op_num].in.y_offset == y_in_initial) {
-                            dim_info[op_idx].res_y_in = @truncate(group_idx);
+                            dim_info[op_idx].res_y_in = group_idx;
                             y_in_reenter = true;
                         }
                     }
                 } else {
                     if (linearized.op[op_start + op_idx + group_idx * op_num].in.y_offset != y_in_initial) {
-                        dim_info[op_idx].wai_y_in = @truncate(group_idx);
+                        dim_info[op_idx].wai_y_in = group_idx;
                         dim_info[op_idx].str_y_in = linearized.op[op_start + op_idx + group_idx * op_num].in.y_offset - y_in_initial;
                         y_in_left = true;
                     }
@@ -229,13 +230,13 @@ pub const Pir = struct {
                 if (x_in_left) {
                     if (!x_in_reenter) {
                         if (linearized.op[op_start + op_idx + group_idx * op_num].in.x_offset == x_in_initial) {
-                            dim_info[op_idx].res_x_in = @truncate(group_idx);
+                            dim_info[op_idx].res_x_in = group_idx;
                             x_in_reenter = true;
                         }
                     }
                 } else {
                     if (linearized.op[op_start + op_idx + group_idx * op_num].in.x_offset != x_in_initial) {
-                        dim_info[op_idx].wai_x_in = @truncate(group_idx);
+                        dim_info[op_idx].wai_x_in = group_idx;
                         dim_info[op_idx].str_x_in = linearized.op[op_start + op_idx + group_idx * op_num].in.x_offset - x_in_initial;
                         x_in_left = true;
                     }
@@ -315,12 +316,12 @@ pub const Pir = struct {
             }
         }
 
-        op_used.* += op_num * group_num;
+        op_used.* += op_num * repeat_num;
 
         return .{
             .op_num = op_num,
             .op = op,
-            .repeat_num = group_num,
+            .repeat_num = repeat_num,
             .dim_info = dim_info,
         };
     }
@@ -328,13 +329,13 @@ pub const Pir = struct {
         allocator.free(this.op);
         allocator.free(this.dim_info);
     }
-    pub fn print(this: *@This(), comptime padding: u32, comptime offset: u32, name: ?[]u8) !void {
+    pub fn print(this: *const @This(), comptime padding: u32, comptime offset: u32, name: ?[]const u8) void {
         if (name) |text| {
             std.debug.print("{s}PIR = {s}\n", .{ " " ** offset, text });
         }
         for (0..this.op_num) |op_idx| {
             std.debug.print("{s}[{}] => ", .{ " " ** (offset + padding), op_idx });
-            try this.op[op_idx].print(0, 0, null);
+            this.op[op_idx].print(0, 0, null);
             std.debug.print("{s}off => out({d:4}) in({d:4})\n", .{
                 " " ** (offset + 2 * padding),
                 this.dim_info[op_idx].off_out,
