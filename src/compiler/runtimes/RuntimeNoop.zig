3ef46412ed46fba6dea42ea65bf5494967fb042e
DatoXx8 <datoxx8@gmail.com>
2025-07-08 14:35:05 +0200

    Add no-op runtime
    Just a runtime that does nothing. Return values for things like
    `runtime.kernelAlloc(...)` are `undefined`.

---
+++ src/compiler/runtimes/RuntimeNoop.zig
@@ -0,0 +1,80 @@
+//! Runtime whose functions just do nothing. Return values are `undefined`.
+
+const std = @import("std");
+const assert = std.debug.assert;
+const Allocator = std.mem.Allocator;
+const builtin = @import("builtin");
+
+const codegen_cl = @import("./codegen_cl.zig");
+const Runtime = @import("./Runtime.zig");
+const Program = @import("../Program.zig");
+const Kernel = Program.Kernel;
+const KernelPtr = Program.KernelPtr;
+const ProgramPtr = Program.ProgramPtr;
+const Memory = Program.Memory;
+const Args = Program.Args;
+const Sync = Program.Sync;
+const Ssa = @import("../Ssa.zig");
+const Assign = Ssa.Assign;
+
+// const opencl_version = @import("opencl_config").opencl_version;
+
+pub const RuntimeNop = @This();
+
+// $TODO This should be removed
+dummy: u32,
+
+pub fn runtime(this: *@This()) Runtime {
+    return Runtime{
+        .state = this,
+        .vtable = .{
+            .init = init,
+            .deinit = deinit,
+            .memoryAlloc = memoryAlloc,
+            .memoryFree = memoryFree,
+            .memorySyncToDevice = memorySyncToDevice,
+            .memorySyncToHost = memorySyncToHost,
+            .programLog = programLog,
+            .programAlloc = programAlloc,
+            .programFree = programFree,
+            .kernelAlloc = kernelAlloc,
+            .kernelFree = kernelFree,
+            .kernelRun = kernelRun,
+            .queueWait = queueWait,
+            .assignCompile = codegen_cl.assignCompile,
+        },
+    };
+}
+
+pub fn init(_: *anyopaque) ?void {}
+pub fn deinit(_: *anyopaque) ?void {}
+pub fn memoryAlloc(_: *anyopaque, _: u32, _: u32, _: u32, _: u32) ?Memory {
+    return undefined;
+}
+pub fn memoryFree(_: *anyopaque, _: Memory) ?void {}
+pub fn memorySyncToHost(_: *anyopaque, _: Memory, _: *anyopaque, _: u32) ?void {}
+pub fn memorySyncToDevice(_: *anyopaque, _: Memory, _: *anyopaque, _: u32) ?void {}
+pub fn programAlloc(_: *anyopaque, _: []const u8) ?ProgramPtr {
+    return undefined;
+}
+pub fn programFree(_: *anyopaque, _: ProgramPtr) ?void {}
+pub fn programLog(_: *anyopaque, _: ProgramPtr, _: Allocator) ?void {}
+pub fn kernelAlloc(_: *anyopaque, _: ProgramPtr, _: [*:0]const u8, _: Args) ?KernelPtr {
+    return undefined;
+}
+pub fn kernelFree(_: *anyopaque, _: KernelPtr) ?void {}
+pub fn kernelRun(_: *anyopaque, _: KernelPtr, _: usize, _: usize) ?void {}
+pub fn queueWait(_: *anyopaque) ?void {}
+pub fn assignCompile(
+    _: *anyopaque,
+    _: Allocator,
+    _: *[]u8,
+    _: *usize,
+    _: Assign,
+    _: []const u8,
+    _: Args,
+    _: u32,
+    _: u32,
+) ?void {
+    @panic("Tried to compile source for no-op runtime\n");
+}
