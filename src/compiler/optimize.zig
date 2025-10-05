bee4f8e632dd54d45b62dd8e7698282cc31dab20
DatoXx8 <datoxx8@gmail.com>
2025-10-05 20:09:20 +0200

    Bunch if fixes

--- src/compiler/optimize.zig
+++ src/compiler/optimize.zig
@@ -439,6 +439,7 @@ pub fn inlineOp(allocator: Allocator, pir: *Pir, left_idx: u32) !void {
                 }
             }
 
+            pir.assign[search_idx].inlined.?.inlined_num = inlined_num_new;
             break;
         } else if (pir.assign[left_idx].base.out.equal(pir.assign[search_idx].base.out)) {
             if (!pir.assign[left_idx].base.out.intermediary) {
@@ -496,6 +497,8 @@ pub fn inlineOp(allocator: Allocator, pir: *Pir, left_idx: u32) !void {
                     pir.assign[search_idx].inlined.?.base[inlined_num_old + inlined_idx] = inlined.base[inlined_idx];
                 }
             }
+
+            pir.assign[search_idx].inlined.?.inlined_num = inlined_num_new;
         } else {
             // Inlining non-intermediaries is only allowed if the target has the same out buffer, which is not the case here
             if (!pir.assign[left_idx].base.out.intermediary) {
@@ -537,9 +540,26 @@ pub fn inlineOp(allocator: Allocator, pir: *Pir, left_idx: u32) !void {
                         }
                     }
                 }
+                pir.assign[search_idx].inlined.?.inlined_num = inlined_num_new;
             }
         }
     }
+
+    var assign_num_new: u32 = 0;
+    var assign_idx: u32 = 0;
+    while (assign_idx < pir.assign_num) : (assign_idx += 1) {
+        if (assign_idx == left_idx) {
+            if (pir.assign[assign_idx].inlined) |*inlined| {
+                allocator.free(inlined.base);
+                allocator.free(inlined.out);
+                allocator.free(inlined.in);
+            }
+        } else {
+            pir.assign[assign_num_new] = pir.assign[assign_idx];
+            assign_num_new += 1;
+        }
+    }
+    pir.assign_num = assign_num_new;
 }
 fn dimInfoMergePossible(left: Assign, right: Assign) bool {
     if ((if (left.inlined) |i| i.inlined_num else 0) != (if (left.inlined) |i| i.inlined_num else 0)) {
@@ -1062,287 +1082,6 @@ pub fn parallelize(allocator: Allocator, pir: *Pir, left_idx: u32, right_idx: u3
     }
     pir.assign_num = assign_num_new;
 }
-// // If you are unlucky with the layout of your offsets then you can get into a situation where the offsets for each assign can't be modeled by a linear function.
-// // This is a huge issue because other functions that model the offsets can't be found easily and table driven solutions limit the loop size artificially,
-// // because of the limits on local memory per kernel.
-// // As a hacky fix we just split the loop up if there is something that can't be modeled linearly. This sucks bad. I hate that I have to do this.
-// // I am sorry for this terrible shittines, I just can't think of a better solution right now
-// fn dimInfoMergePossible(base: Assign, merge: Assign) bool {
-//     if ((if (base.inlined) |i| i.inlined_num else 0) != (if (merge.inlined) |i| i.inlined_num else 0)) {
-//         return false;
-//     }
-//
-//     const base_num: u32 = 1 + if (base.inlined) |i| i.inlined_num else 0;
-//
-//     for (0..base_num) |base_idx| {
-//         const pre: Base = if (base_idx == 0) base.base else base.inlined.?.base[base_idx - 1];
-//         const post: Base = if (base_idx == 0) merge.base else merge.inlined.?.base[base_idx - 1];
-//         if (!pre.equalNoOffset(post)) {
-//             return false;
-//         }
-//
-//         inline for (0..8) |dim_idx| {
-//             const wait: u32 = switch (dim_idx) {
-//                 0 => pre.out_dim.a_wait,
-//                 1 => pre.out_dim.z_wait,
-//                 2 => pre.out_dim.y_wait,
-//                 3 => pre.out_dim.x_wait,
-//                 4 => pre.in_dim.a_wait,
-//                 5 => pre.in_dim.z_wait,
-//                 6 => pre.in_dim.y_wait,
-//                 7 => pre.in_dim.x_wait,
-//                 else => unreachable,
-//             };
-//             const stride: u32 = switch (dim_idx) {
-//                 0 => pre.out_dim.a_stride,
-//                 1 => pre.out_dim.z_stride,
-//                 2 => pre.out_dim.y_stride,
-//                 3 => pre.out_dim.x_stride,
-//                 4 => pre.in_dim.a_stride,
-//                 5 => pre.in_dim.z_stride,
-//                 6 => pre.in_dim.y_stride,
-//                 7 => pre.in_dim.x_stride,
-//                 else => unreachable,
-//             };
-//             const reset: u32 = switch (dim_idx) {
-//                 0 => pre.out_dim.a_reset,
-//                 1 => pre.out_dim.z_reset,
-//                 2 => pre.out_dim.y_reset,
-//                 3 => pre.out_dim.x_reset,
-//                 4 => pre.in_dim.a_reset,
-//                 5 => pre.in_dim.z_reset,
-//                 6 => pre.in_dim.y_reset,
-//                 7 => pre.in_dim.x_reset,
-//                 else => unreachable,
-//             };
-//             const off_pre: u32 = switch (dim_idx) {
-//                 0 => pre.out.aOffset(),
-//                 1 => pre.out.zOffset(),
-//                 2 => pre.out.yOffset(),
-//                 3 => pre.out.xOffset(),
-//                 4 => pre.in.aOffset(),
-//                 5 => pre.in.zOffset(),
-//                 6 => pre.in.yOffset(),
-//                 7 => pre.in.xOffset(),
-//                 else => unreachable,
-//             };
-//             const off_merge: u32 = switch (dim_idx) {
-//                 0 => post.out.aOffset(),
-//                 1 => post.out.zOffset(),
-//                 2 => post.out.yOffset(),
-//                 3 => post.out.xOffset(),
-//                 4 => post.in.aOffset(),
-//                 5 => post.in.zOffset(),
-//                 6 => post.in.yOffset(),
-//                 7 => post.in.xOffset(),
-//                 else => unreachable,
-//             };
-//
-//             if (off_merge < off_pre) {
-//                 return false;
-//             }
-//
-//             if (wait == DimInfo.value_none) {
-//                 assert(stride == DimInfo.value_none);
-//             } else {
-//                 assert(stride != DimInfo.value_none);
-//                 if (reset == DimInfo.value_none) {
-//                     if (off_pre != off_merge and @divFloor(pre.repeats, wait) * stride != (off_merge - off_pre)) {
-//                         return false;
-//                     }
-//                 } else {
-//                     if (@divFloor((pre.repeats) % reset, wait) * stride != (off_merge - off_pre)) {
-//                         return false;
-//                     }
-//                 }
-//             }
-//         }
-//     }
-//
-//     return true;
-// }
-// fn dimInfoMerge(base: Assign, merge: *Assign) void {
-//     assert((base.inlined == null) == (merge.inlined == null));
-//     assert((if (base.inlined) |i| i.inlined_num else 0) == (if (merge.inlined) |i| i.inlined_num else 0));
-//     // assert(dimInfoMergePossible(base, merge.*)); // Might be a bit slow to check every time
-//
-//     const base_num: u32 = 1 + if (base.inlined) |i| i.inlined_num else 0;
-//
-//     for (0..base_num) |base_idx| {
-//         const pre: *const Base = if (base_idx == 0) &base.base else &base.inlined.?.base[base_idx - 1];
-//         const post: *Base = if (base_idx == 0) &merge.base else &merge.inlined.?.base[base_idx - 1];
-//
-//         inline for (0..2) |buffer_idx| {
-//             const pre_buffer: Buffer = if (buffer_idx == 0) pre.out else pre.in;
-//             const post_buffer: Buffer = if (buffer_idx == 0) post.out else post.in;
-//             var modified: DimInfo = if (buffer_idx == 0) pre.out_dim else pre.in_dim;
-//             inline for (0..4) |dim_idx| {
-//                 const wait: *u32 = switch (dim_idx) {
-//                     0 => &modified.a_wait,
-//                     1 => &modified.z_wait,
-//                     2 => &modified.y_wait,
-//                     3 => &modified.x_wait,
-//                     else => unreachable,
-//                 };
-//                 const stride: *u32 = switch (dim_idx) {
-//                     0 => &modified.a_stride,
-//                     1 => &modified.z_stride,
-//                     2 => &modified.y_stride,
-//                     3 => &modified.x_stride,
-//                     else => unreachable,
-//                 };
-//                 const reset: *u32 = switch (dim_idx) {
-//                     0 => &modified.a_reset,
-//                     1 => &modified.z_reset,
-//                     2 => &modified.y_reset,
-//                     3 => &modified.x_reset,
-//                     else => unreachable,
-//                 };
-//                 const off_pre: u32 = switch (dim_idx) {
-//                     0 => pre_buffer.aOffset(),
-//                     1 => pre_buffer.zOffset(),
-//                     2 => pre_buffer.yOffset(),
-//                     3 => pre_buffer.xOffset(),
-//                     else => unreachable,
-//                 };
-//                 const off_post: u32 = switch (dim_idx) {
-//                     0 => post_buffer.aOffset(),
-//                     1 => post_buffer.zOffset(),
-//                     2 => post_buffer.yOffset(),
-//                     3 => post_buffer.xOffset(),
-//                     else => unreachable,
-//                 };
-//                 if (wait.* == DimInfo.value_none) {
-//                     assert(stride.* == DimInfo.value_none);
-//                     if (off_pre != off_post) {
-//                         assert(off_pre < off_post);
-//                         wait.* = pre.repeats;
-//                         stride.* = off_post - off_pre;
-//                     }
-//                 } else {
-//                     assert(stride.* != DimInfo.value_none);
-//                     if (reset.* == DimInfo.value_none) {
-//                         if (off_pre == off_post) {
-//                             reset.* = pre.repeats;
-//                         }
-//                     }
-//                 }
-//             }
-//
-//             if (buffer_idx == 0) {
-//                 post.out_dim = modified;
-//             } else {
-//                 post.in_dim = modified;
-//             }
-//         }
-//
-//         post.out = pre.out;
-//         post.in = pre.in;
-//         post.repeats = pre.repeats + 1;
-//     }
-// }
-// // Assumes `this` and `base` hold the base offsets
-// fn dimInfoOverlap(this: Buffer, this_dim: DimInfo, this_repeats: u32, target: Buffer, target_dim: DimInfo, target_repeats: u32) bool {
-//     const this_a_reset: u32 = if (this_dim.a_reset == DimInfo.value_none) DimInfo.reset_default else this_dim.a_reset;
-//     const this_z_reset: u32 = if (this_dim.z_reset == DimInfo.value_none) DimInfo.reset_default else this_dim.z_reset;
-//     const this_y_reset: u32 = if (this_dim.y_reset == DimInfo.value_none) DimInfo.reset_default else this_dim.y_reset;
-//     const this_x_reset: u32 = if (this_dim.x_reset == DimInfo.value_none) DimInfo.reset_default else this_dim.x_reset;
-//     const this_a_wait: u32 = if (this_dim.a_wait == DimInfo.value_none) DimInfo.wait_default else this_dim.a_wait;
-//     const this_z_wait: u32 = if (this_dim.z_wait == DimInfo.value_none) DimInfo.wait_default else this_dim.z_wait;
-//     const this_y_wait: u32 = if (this_dim.y_wait == DimInfo.value_none) DimInfo.wait_default else this_dim.y_wait;
-//     const this_x_wait: u32 = if (this_dim.x_wait == DimInfo.value_none) DimInfo.wait_default else this_dim.x_wait;
-//     const this_a_stride: u32 = if (this_dim.a_stride == DimInfo.value_none) DimInfo.stride_default else this_dim.a_stride;
-//     const this_z_stride: u32 = if (this_dim.z_stride == DimInfo.value_none) DimInfo.stride_default else this_dim.z_stride;
-//     const this_y_stride: u32 = if (this_dim.y_stride == DimInfo.value_none) DimInfo.stride_default else this_dim.y_stride;
-//     const this_x_stride: u32 = if (this_dim.x_stride == DimInfo.value_none) DimInfo.stride_default else this_dim.x_stride;
-//
-//     const target_a_reset: u32 = if (target_dim.a_reset == DimInfo.value_none) DimInfo.reset_default else target_dim.a_reset;
-//     const target_z_reset: u32 = if (target_dim.z_reset == DimInfo.value_none) DimInfo.reset_default else target_dim.z_reset;
-//     const target_y_reset: u32 = if (target_dim.y_reset == DimInfo.value_none) DimInfo.reset_default else target_dim.y_reset;
-//     const target_x_reset: u32 = if (target_dim.x_reset == DimInfo.value_none) DimInfo.reset_default else target_dim.x_reset;
-//     const target_a_wait: u32 = if (target_dim.a_wait == DimInfo.value_none) DimInfo.wait_default else target_dim.a_wait;
-//     const target_z_wait: u32 = if (target_dim.z_wait == DimInfo.value_none) DimInfo.wait_default else target_dim.z_wait;
-//     const target_y_wait: u32 = if (target_dim.y_wait == DimInfo.value_none) DimInfo.wait_default else target_dim.y_wait;
-//     const target_x_wait: u32 = if (target_dim.x_wait == DimInfo.value_none) DimInfo.wait_default else target_dim.x_wait;
-//     const target_a_stride: u32 = if (target_dim.a_stride == DimInfo.value_none) DimInfo.stride_default else target_dim.a_stride;
-//     const target_z_stride: u32 = if (target_dim.z_stride == DimInfo.value_none) DimInfo.stride_default else target_dim.z_stride;
-//     const target_y_stride: u32 = if (target_dim.y_stride == DimInfo.value_none) DimInfo.stride_default else target_dim.y_stride;
-//     const target_x_stride: u32 = if (target_dim.x_stride == DimInfo.value_none) DimInfo.stride_default else target_dim.x_stride;
-//
-//     var this_idx: u32 = 0;
-//     while (this_idx < this_repeats) : (this_idx += 1) {
-//         const a_1: u32 = this.aOffset() + (this_idx % this_a_reset) / this_a_wait * this_a_stride;
-//         const z_1: u32 = this.zOffset() + (this_idx % this_z_reset) / this_z_wait * this_z_stride;
-//         const y_1: u32 = this.yOffset() + (this_idx % this_y_reset) / this_y_wait * this_y_stride;
-//         const x_1: u32 = this.xOffset() + (this_idx % this_x_reset) / this_x_wait * this_x_stride;
-//
-//         var target_idx: u32 = 0;
-//         while (target_idx < target_repeats) : (target_idx += 1) {
-//             const a_2: u32 = target.aOffset() + (target_idx % target_a_reset) / target_a_wait * target_a_stride;
-//             const z_2: u32 = target.zOffset() + (target_idx % target_z_reset) / target_z_wait * target_z_stride;
-//             const y_2: u32 = target.yOffset() + (target_idx % target_y_reset) / target_y_wait * target_y_stride;
-//             const x_2: u32 = target.xOffset() + (target_idx % target_x_reset) / target_x_wait * target_x_stride;
-//             const overlap: bool = @max(a_1, a_2) < @min(a_1 + this.a_size, a_2 + target.a_size) and
-//                 @max(z_1, z_2) < @min(z_1 + this.z_size, z_2 + target.z_size) and
-//                 @max(y_1, y_2) < @min(y_1 + this.y_size, y_2 + target.y_size) and
-//                 @max(x_1, x_2) < @min(x_1 + this.x_size, x_2 + target.x_size);
-//             if (overlap) {
-//                 return true;
-//             }
-//         }
-//     }
-//     return false;
-// }
-// pub fn parallelizeGather(allocator: Allocator, optimization: *[]Optimization, optimization_count: *u32, pir: Pir) !void {
-//     var start_idx: u32 = 0;
-//     while (start_idx < pir.assign_num - 1) : (start_idx += 1) {
-//         var search_idx: u32 = start_idx + 1;
-//         while (search_idx < pir.assign_num) : (search_idx += 1) {
-//             // I think these conditions are the least restrictive they could be because of the inlining that happens before parallelization
-//             // At the very least it is not obvious to me how to loosen them.
-//             const overlap_out_out: bool = pir.assign[start_idx].base.out.id == pir.assign[search_idx].base.out.id and
-//                 dimInfoOverlap(pir.assign[start_idx].base.out, pir.assign[start_idx].base.out_dim, pir.assign[start_idx].base.repeats, //
-//                     pir.assign[search_idx].base.out, pir.assign[search_idx].base.out_dim, pir.assign[search_idx].base.repeats);
-//             const overlap_out_in: bool = pir.assign[start_idx].base.out.id == pir.assign[search_idx].base.in.id and
-//                 dimInfoOverlap(pir.assign[start_idx].base.out, pir.assign[start_idx].base.out_dim, pir.assign[start_idx].base.repeats, //
-//                     pir.assign[search_idx].base.in, pir.assign[search_idx].base.in_dim, pir.assign[search_idx].base.repeats);
-//             const overlap_in_out: bool = pir.assign[start_idx].base.in.id == pir.assign[search_idx].base.out.id and
-//                 dimInfoOverlap(pir.assign[start_idx].base.in, pir.assign[start_idx].base.in_dim, pir.assign[start_idx].base.repeats, //
-//                     pir.assign[search_idx].base.out, pir.assign[search_idx].base.out_dim, pir.assign[search_idx].base.repeats);
-//             const overlap_inline: bool = blk: {
-//                 if (pir.assign[search_idx].inlined) |inlined| {
-//                     var inlined_idx: u32 = 0;
-//                     while (inlined_idx < inlined.inlined_num) : (inlined_idx += 1) {
-//                         if (pir.assign[start_idx].base.out.id == inlined.base[inlined_idx].in.id and inlined.in[inlined_idx] == null and
-//                             dimInfoOverlap(pir.assign[start_idx].base.out, pir.assign[start_idx].base.out_dim, pir.assign[start_idx].base.repeats, //
-//                                 inlined.base[inlined_idx].in, inlined.base[inlined_idx].in_dim, inlined.base[inlined_idx].repeats))
-//                         {
-//                             break :blk true;
-//                         }
-//                     }
-//                 }
-//                 break :blk false;
-//             };
-//             if (overlap_out_out or overlap_out_in or overlap_in_out or overlap_inline) {
-//                 break;
-//             }
-//
-//             if (dimInfoMergePossible(pir.assign[start_idx], pir.assign[search_idx])) {
-//                 defer optimization_count.* += 1;
-//                 if (optimization_count.* == optimization.*.len) {
-//                     optimization.* = try allocator.realloc(optimization.*, optimization.*.len * 2);
-//                 }
-//                 optimization.*[optimization_count.*] = .{
-//                     .parallelize = .{
-//                         .left_idx = start_idx,
-//                         .right_idx = search_idx,
-//                     },
-//                 };
-//                 break;
-//             }
-//         }
-//     }
-// }
 // $TODO Add in local size as a factor because those are also likely to have some cache coherency
 pub fn splitKernelGather(
     allocator: Allocator,
@@ -1356,7 +1095,8 @@ pub fn splitKernelGather(
     var assign_idx: u32 = 0;
     while (assign_idx < pir.assign_num) : (assign_idx += 1) {
         if (!pir.assign[assign_idx].base.kind.isReduce() and
-            pir.assign[assign_idx].base.repeats < size_global)
+            pir.assign[assign_idx].base.repeats < size_global and
+            !pir.assign[assign_idx].split)
         {
             defer optimization_count.* += 1;
             if (optimization_count.* == optimization.*.len) {
